aea04906dbd01f526e8cef3eb7f8d3353f2c851f
Daniel Howorth <daniel.howorth@sky.com>
2025-03-21 14:50:52 +0000

    added from markup and created controlled input

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [guessValue, setGuessValue] = React.useState("");
+
+  return (
+    <form
+      class="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.info(guessValue);
+      }}
+    >
+      <label for="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guessValue}
+        onChange={(event) => {
+          setGuessValue(event.target.value);
+        }}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
