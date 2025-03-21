e62f4d54a536e0d61e84f810b904f0db4966a9fc
Daniel Howorth <daniel.howorth@sky.com>
2025-03-21 23:30:23 +0000

    updated Guess to use Cell component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,18 +3,25 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ value, status }) {
+  const className = status ? `cell ${status}` : "cell";
+
+  return <span className={className}>{value}</span>;
+}
+
 function Guess({ guessValue, answer }) {
   // const chars = guessValue ? guessValue.split("") : Array(5).fill("");
 
-  const guessResults = checkGuess(guessValue, answer) || [];
-  console.log(guessResults);
+  const result = checkGuess(guessValue, answer);
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className={`cell ${guessResults[num]?.status}`}>
-          {guessValue ? guessValue[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          value={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
 
